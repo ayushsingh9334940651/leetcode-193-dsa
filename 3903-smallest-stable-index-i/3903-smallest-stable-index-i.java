28d14f627798365d2da6f8e6393a824fdf895a41
Ayush raj <ayushsingh9334940651@gmail.com>
2026-09-04 17:29:08 +0530

    Time: 1 ms (99.79%), Space: 46.5 MB (32.13%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,15 +1,18 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        if(nums.length==1 && k>=0) return 0;
-        //int ans=-1;
-      PriorityQueue<Integer> pq=new PriorityQueue<>();
-      for(int ele: nums) pq.add(ele);
-      int max=Integer.MIN_VALUE;
-      for(int i=0;i<nums.length;i++){
-       max=Math.max(max,nums[i]);
-       if(max-pq.peek()<=k) return i;
-       pq.remove(nums[i]);
-      }
+     if(nums.length==1 && k>=0) return 0;
+     int n=nums.length;
+     int pre[]=new int[n];
+     int min=nums[n-1];
+     for(int i=n-1;i>=0;i--){
+     if(nums[i]<min) min=nums[i];
+     pre[i]=Math.min(min,nums[i]);
+     }
+     int max=Integer.MIN_VALUE;
+     for(int i=0;i<n;i++){
+     max=Math.max(max,nums[i]);
+     if(max-pre[i]<=k) return i;
+     }
      return -1;
     }
 }
