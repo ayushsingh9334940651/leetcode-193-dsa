66d389e4071fa6289b7734d8019e6d275b9f74e3
Rawal Ayush Singh <ayushsingh9334940651@gmail.com>
2026-09-06 02:24:19 +0530

    Time: 0 ms (100%), Space: 43.7 MB (90.7%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,35 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         int n=matrix.length-1;
-         int m=matrix[0].length-1;
-         int rstart=0;
-        
-         while(rstart<=n){
-            int mid=rstart+(n-rstart)/2;
-          if(target >= matrix[mid][0] && target <= matrix[mid][m]){
-                 int start=0;
-                 int endcol=m;
-             while(start<=endcol){
-                 
-                int midc=start+(endcol-start)/2;
-                if(target==matrix[mid][midc]){
-                    return true;
-                }else if(target>matrix[mid][midc]){
-                    start=midc+1;
-                }else{
-                    endcol=midc-1;
-                }
-                
+        int row=0;int lastrow=matrix.length-1;
+        int col=0;int lastcol=matrix[0].length-1;
+        while(row<=lastrow){
+            int mid=row+(lastrow-row)/2;
+            if(matrix[mid][0]>target) lastrow=mid-1;
+            else if(matrix[mid][lastcol]<target) row=mid+1;
+            else{
+             while(col<=lastcol){
+                int middle=col+(lastcol-col)/2;
+                if(matrix[mid][middle]<target) col=middle+1;
+                else if(matrix[mid][middle]>target) lastcol=middle-1;
+                else return true;
              }
-              return false; 
-              }
-               else if(target>matrix[mid][m]){
-                 rstart=mid+1;
-
-               }else{
-                  n=mid-1;
-               }
+             }
          }
-         return false;
+    return false;
     }
 }
