804bf938c7c3dcdc22e6c43db62f562847f1eca4
Rawal Ayush Singh <ayushsingh9334940651@gmail.com>
2026-09-08 00:20:55 +0530

    Time: 1 ms (96.74%), Space: 47.8 MB (24.6%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,32 +1,51 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-     
-        boolean row[]=new boolean[matrix.length];
-        boolean col[]=new boolean [matrix[0].length];
-       
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-              if(matrix[i][j]==0){
-                row[i]=true;
-                col[j]=true;
-              }
+        boolean firstrow=false;
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                 firstrow=true;
+                 break;
             }
         }
-         for(int i=0;i<matrix.length;i++){
-             if(row[i]==true){
-                int l=0;
-             while(l<matrix[0].length){
-                matrix[i][l++]=0;
-             }
+        boolean firstcol=false;
+        for(int j=0;j<matrix.length;j++){
+            if(matrix[j][0]==0){
+                 firstcol=true;
+                 break;
             }
-            for(int j=0;j<matrix[0].length;j++){
-              if(col[j]==true){
-                int k=0;
-                while(k<matrix.length){
-                    matrix[k++][j]=0;
-                }
-              }
+        }
+      for(int i=1;i<matrix.length;i++){
+         for(int j=1;j<matrix[0].length;j++){
+          if(matrix[i][j]==0){
+            matrix[i][0]=0;
+            matrix[0][j]=0;
+          }   
+
+         }
+        }
+     for(int i=1;i<matrix.length;i++){
+        if(matrix[i][0]==0){
+         for(int j=1;j<matrix[0].length;j++){
+            matrix[i][j]=0;
+         }
+        }
+     }
+     for(int j=1;j<matrix[0].length;j++){
+        if(matrix[0][j]==0){
+            for(int i=1;i<matrix.length;i++){
+                matrix[i][j]=0;
             }
-        }  
-      }
+        }
+     }
+     if(firstrow==true){
+        for(int j=0;j<matrix[0].length;j++){
+         matrix[0][j]=0;
+       }
+     }
+     if(firstcol==true){
+        for(int i=0;i<matrix.length;i++){
+            matrix[i][0]=0;
+        }
+     }
+    }
 }
