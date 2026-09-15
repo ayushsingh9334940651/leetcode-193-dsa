2d193f0b473b67efc913df2ada9172f8b7067faf
Rawal Ayush Singh <ayushsingh9334940651@gmail.com>
2026-09-16 01:44:52 +0530

    Time: 7 ms (71.76%), Space: 49.6 MB (60.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,51 +1,17 @@
 class Solution {
-    private boolean isanagram(String s1,String s2){
-        if(s1.length()!=s2.length()) return false;
-        HashMap<Character,Integer> hs1=new HashMap<>();
-        HashMap<Character,Integer> hs2=new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-         char ch=s1.charAt(i);
-         if(hs1.containsKey(ch)){
-            int frq=hs1.get(ch);
-         hs1.put(ch,frq+1);
-         }
-         else hs1.put(ch,1);
-        }
-        for(int i=0;i<s2.length();i++){
-         char ch=s2.charAt(i);
-         if(!hs1.containsKey(ch)) return false;
-         if(hs2.containsKey(ch)){
-            int freq=hs2.get(ch);
-            hs2.put(ch,freq+1);
-         }else hs2.put(ch,1);
-        }
-     for(int i=0;i<s2.length();i++){
-        char ch=s1.charAt(i);
-        int freq1=hs1.get(ch);
-        int freq2=hs2.getOrDefault(ch, 0);
-        if(freq1!=freq2) return false;
-     }
-     return true;
-    }
+    
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashSet<Integer> set=new HashSet<>();
-        ArrayList<List<String>> ans=new ArrayList<>();
-        for(int i=0;i<strs.length;i++){
-         if(!set.contains(i)){
-         ArrayList<String> help=new ArrayList<>();
-             help.add(strs[i]);
-             set.add(i);
-             int j=i+1;
-         while(j<strs.length){
-          if(isanagram(strs[i],strs[j]) && !set.contains(j)){
-            help.add(strs[j]);
-            set.add(j);
-          }
-          j++;
-         }
-          ans.add(help);
-        }
-        }
-        return ans;
+     if(strs.length==1) return new ArrayList<>(Arrays.asList(Arrays.asList(strs[0])));
+     HashMap<String,List<String>> map=new HashMap<>();
+     for(String str:strs){
+      char [] ch=str.toCharArray();
+      Arrays.sort(ch);
+      String sortedstr=new String(ch);
+      if(!map.containsKey(sortedstr)){
+        map.put(sortedstr,new ArrayList<>());
+      }
+      map.get(sortedstr).add(str);
+     }
+     return new ArrayList<>(map.values());
     }
 }
