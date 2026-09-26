4d71795faff88d6662f1251cbd3ec420f6c52264
Rawal Ayush Singh <ayushsingh9334940651@gmail.com>
2026-09-27 01:08:16 +0530

    Time: 2 ms (81.88%), Space: 46.5 MB (80.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,75 +1,46 @@
 class Solution {
-    static List<List<String>> ans = new ArrayList<>();
-
-    public static boolean check(char arr[][],int index,int col){
-     for(int i=index-1;i>=0;i--){
-        if(arr[i][col]=='Q'){
-           return false;
-        }
-     }
-     int i=index-1;
-     int j=col-1;
-
-     while(i>=0 && j>=0){
-        if(arr[i][j]=='Q'){
-            return false;
-        }
-        i--;
-        j--;
-     }
-    
-     j=col+1;
-     i=index-1;
-     while(i>=0 && j<arr.length){
-        if(arr[i][j]=='Q'){
-            return false;
-        }
-        i--;
-        j++;
-     }
-     return true;
-
+    private boolean check(char n[][],int row,int col){
+      for(int i=row-1;i>=0;i--){
+        if(n[i][col]=='Q') return false;
+      }
+      int i=row-1,j=col-1;
+      while(i>=0 && j>=0){
+        if(n[i][j]=='Q') return false;
+        i--;j--;
+      }
+      i=row-1; j=col+1;
+      while(i>=0 && j<n.length){
+        if(n[i][j]=='Q') return false;
+        i--;j++;
+      }
+      return true;
     }
-    public static void nqueen(char n[][],int row){
-        if(row==n.length){
-           List<String> temp = new ArrayList<>();
-
-            for (int i = 0; i < n.length; i++) {
-                temp.add(new String(n[i]));
-            }
-
-            ans.add(temp);
-            return;
+    private void nqueen(char n[][],int row,List<List<String>> ans){
+     if(row==n.length){
+        ArrayList<String> help=new ArrayList<>();
+        for(int i=0;i<n.length;i++){
+            help.add(new String(n[i]));
         }
-       
-        for(int j=0;j<n.length;j++){
-            if(check(n,row,j)){
-            n[row][j]='Q';
-            nqueen(n,row+1);
-            n[row][j]='.';
-        }
-       }
-
+        ans.add(help);
+        return ;
+     }
+     for(int j=0;j<n.length;j++){
+      if(check(n,row,j)){
+       n[row][j]='Q';
+       nqueen(n,row+1,ans);
+       n[row][j]='.';
+      }
+      }
     }
-    public static void printarr(char arr[][]){
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
     public List<List<String>> solveNQueens(int n) {
-         ans.clear();
-
+      List<List<String>> ans = new ArrayList<>();
         char arr[][]=new char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 arr[i][j]='.';
             }
         }
-        nqueen(arr,0);
+        nqueen(arr,0,ans);
       
       return ans;
         
