2a5e8d0b504ae9b23d65f4c29319bb51ecca4dc4
Rawal Ayush Singh <ayushsingh9334940651@gmail.com>
2026-09-27 01:43:44 +0530

    Time: 3 ms (99.58%), Space: 44.8 MB (93.17%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,4 +1,31 @@
 class Solution {
+ private void mergesort(int arr[],int low,int high){
+     if(low>=high) return ;
+    
+     int mid=low+(high-low)/2;
+     mergesort(arr,low,mid);
+     mergesort(arr,mid+1,high);
+     merge(arr,low,mid,high);
+    }
+ private void merge(int arr[],int low,int mid,int high){
+    int i=low; int j=mid+1;
+    int temp[]=new int[high-low+1];
+    int k =0;
+  while(i<=mid && j<=high){
+     if(arr[i]<=arr[j] ) temp[k++]=arr[i++];
+     else temp[k++]=arr[j++];
+  }
+  while(i<=mid){
+    temp[k++]=arr[i++];
+
+  }
+  while(j<=high){
+    temp[k++]=arr[j++];
+  }
+  for(int v=0;v<temp.length;v++){
+    arr[low+v]=temp[v];
+  }
+}
     private void solution(int []candidates,int target,int indx, ArrayList<List<Integer>>result, ArrayList<Integer> help){
       if(target==0){
         result.add(new ArrayList<>(help));
@@ -14,7 +41,7 @@ class Solution {
       solution(candidates,target,indx+1,result,help);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
+        mergesort(candidates,0,candidates.length-1);
       ArrayList<List<Integer>>result= new ArrayList<>();
       ArrayList<Integer> help=new ArrayList<>(); 
       solution(candidates,target,0,result,help);
